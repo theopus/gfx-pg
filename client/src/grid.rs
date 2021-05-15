2ad55298cbe51f0186e192bd89668e6157df7c1a
theopus <theopus5@gmail.com>
2021-05-15 19:12:32 +0300

    grid

--- client/src/grid.rs
+++ client/src/grid.rs
@@ -9,11 +9,10 @@ use rx::glm;
 
 use crate::glm::Vec2;
 
-struct Grid2D<V> {
+struct Grid2D {
     step: f32,
     dimensions: u32,
     depth: u32,
-    data: HashMap<Grid2DCell, V>,
 
     dimensions_x: u32,
     dimensions_y: u32,
@@ -26,7 +25,7 @@ struct Grid2DCell {
     y: u32,
 }
 
-impl<V> Grid2D<V> {
+impl Grid2D {
     fn get_level_step(&self, level: u32) -> (f32, f32) {
         return if level == 0 {
             (self.step, self.step)
@@ -38,19 +37,6 @@ impl<V> Grid2D<V> {
         };
     }
 
-    pub fn put_data(&mut self, level0: (u32, u32), offset: (u32, u32, u32), data: V) {
-        let (cell_per_level_x, cell_per_level_y) = self.cells_per(offset.2);
-        self.put_data_0(
-            (level0.0 + offset.0 * cell_per_level_x,
-             level0.1 + offset.1 * cell_per_level_y),
-            data,
-        )
-    }
-
-    fn put_data_0(&mut self, (x, y): (u32, u32), data: V) {
-        info!("put to: {:?}", (x, y));
-        self.data.insert(Grid2DCell { x, y }, data);
-    }
 
     pub fn cells_per(&self, level: u32) -> (u32, u32) {
         (
@@ -76,25 +62,31 @@ impl<V> Grid2D<V> {
     }
 
     fn is_in_bounds(&self, (x, y): (i32, i32), level: u32) -> bool {
-        let (max_x, max_y) = self.max_cell_at(level);
-        x >= 0 && x < max_x as i32
-            && y >= 0 && y < max_y as i32
+        if let Some(bounds) = self.bounds {
+            let (max_x, max_y) = self.max_cell_at(level);
+            return x >= 0 && x < max_x as i32
+                && y >= 0 && y < max_y as i32;
+        }
+        true
     }
 
-    pub fn get_cell_at(&self, origin: &glm::Vec2, point: &glm::Vec2, level: u32) -> Option<glm::UVec2> {
+    pub fn get_cell_at(&self, origin: &glm::Vec2, point: &glm::Vec2, level: u32) -> Option<glm::IVec2> {
         let relative_point: glm::Vec2 = point.borrow() - origin.borrow();
         let (x_level_step, y_level_step) = self.get_level_step(level);
-        let x = (relative_point.x / x_level_step) as i32;
-        let y = (relative_point.y / y_level_step) as i32;
+        let mut x = (relative_point.x / x_level_step);
+        let mut y = (relative_point.y / y_level_step);
 
-        if self.is_in_bounds((x, y), level) {
-            return Some(glm::vec2(x as u32, y as u32));
+        if x < 0. { x=x.floor()};
+        if y < 0. { y=y.floor()};
+
+        if self.is_in_bounds((x as i32, y as i32), level) {
+            return Some(glm::vec2(x as i32, y as i32));
         }
         None
     }
 
     ///returns center of cell
-    pub fn pos_for_cell_at(&self, origin: &glm::Vec2, (x, y): (u32, u32), level: u32) -> Option<glm::Vec2> {
+    pub fn pos_for_cell_at(&self, origin: &glm::Vec2, (x, y): (i32, i32), level: u32) -> Option<glm::Vec2> {
         if !self.is_in_bounds((x as i32, y as i32), level) {
             return None;
         }
@@ -108,7 +100,7 @@ impl<V> Grid2D<V> {
     }
 
     ///returns top left of cell
-    pub fn pos_for_cell_start(&self, origin: &glm::Vec2, (x, y): (u32, u32), level: u32) -> Option<glm::Vec2> {
+    pub fn pos_for_cell_start(&self, origin: &glm::Vec2, (x, y): (i32, i32), level: u32) -> Option<glm::Vec2> {
         if !self.is_in_bounds((x as i32, y as i32), level) {
             return None;
         }
@@ -120,13 +112,12 @@ impl<V> Grid2D<V> {
     }
 }
 
-impl<V> Grid2D<V> {
+impl Grid2D {
     pub fn new_symmetrical(step: f32, dimensions: u32, depth: u32) -> Self {
         Self {
             step,
             dimensions,
             depth,
-            data: HashMap::with_capacity(dimensions.pow(2) as usize),
             dimensions_x: dimensions,
             dimensions_y: dimensions,
             bounds: None,
@@ -144,7 +135,6 @@ impl<V> Grid2D<V> {
             step,
             dimensions: max,
             depth,
-            data: HashMap::with_capacity(max.pow(2) as usize),
             dimensions_x: dimensions.0,
             dimensions_y: dimensions.1,
             bounds,
@@ -155,17 +145,14 @@ impl<V> Grid2D<V> {
 #[test]
 fn test() {
     crate::init_log();
-    let mut d: Grid2D<u32> = Grid2D::new_asymmetrical(2., (5, 10), 2, Some(((1, 10_000), 1)));
+    let mut d: Grid2D = Grid2D::new_asymmetrical(2., (5, 5), 1, None);
 
-    d.get_cell_at(&glm::vec2(0., 0.), &glm::vec2(9.0, 2.5), 0)
+    d.get_cell_at(&glm::vec2(0., 0.), &glm::vec2(-2.6, -2.5), 0)
         .map(|p| info!("0 {:?}", p));
-    d.get_cell_at(&glm::vec2(0., 0.), &glm::vec2(9.0, 2.5), 2)
-        .map(|p| info!("2 {:?}", p));
-    d.get_cell_at(&glm::vec2(0., 0.), &glm::vec2(9.0, 2.5), 1)
-        .map(|p| info!("1 {:?}", p));
-    info!("max for cell:");
 
-    for i in 0..3 {
-        info!("l: {:?} = {:?}", i, &d.max_cell_at(i))
-    }
+    d.pos_for_cell_at(&glm::vec2(0., 0.), (-1, -1), 0)
+        .map(|p| info!("0 {:?}", p));
+
+    d.pos_for_cell_start(&glm::vec2(0., 0.), (-1, -1), 0)
+        .map(|p| info!("0 {:?}", p));
 }
